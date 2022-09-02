ba6b09089907e1de528fd1a9c9cd930eab4b99eb
yt-topia <112598719+yt-topia@users.noreply.github.com>
2022-09-02 10:13:43 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,15 @@
+public abstract class TnagibleAsset{
+  private String name;
+  private int price;
+  private String color;
+  
+  public TangibleAsset(String name, int price, String color){
+    this.name = nema;
+    this.price = price;
+    this.color = color;
+  }
+  
+  public String getName(){return this.name;}
+  public int getPrice(){return this.price;}
+  public String getColor(){return this.color;}
+}
